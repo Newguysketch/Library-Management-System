1d2df0d5d8b9278ba30e2466811c4119ba7e7d48
Suyog Basukala <basukalas1@gmail.com>
2026-06-07 16:19:06 +1000

    Added member management functionality

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -11,23 +11,50 @@ class Book {
         this.author = author;
     }
 
+    @Override
     public String toString() {
         return isbn + " | " + title + " | " + author;
+    }
+}
+
+class Member {
+    String memberId;
+    String name;
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return memberId + " | " + name;
     }
 }
 
 public class LibraryManagementSystem {
 
     static ArrayList<Book> books = new ArrayList<>();
+    static ArrayList<Member> members = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        books.add(new Book("101", "Java Basics", "James"));
-        books.add(new Book("102", "OOP Concepts", "Robert"));
+        // Books
+        books.add(new Book("B001", "Java Basics", "James Gosling"));
+        books.add(new Book("B002", "OOP Concepts", "Robert Martin"));
 
-        System.out.println("Library Books:");
+        // Members
+        members.add(new Member("M001", "Suyog Basukala"));
+        members.add(new Member("M002", "John Smith"));
+
+        System.out.println("===== LIBRARY BOOKS =====");
         for (Book book : books) {
             System.out.println(book);
+        }
+
+        System.out.println("\n===== LIBRARY MEMBERS =====");
+        for (Member member : members) {
+            System.out.println(member);
         }
     }
 }
